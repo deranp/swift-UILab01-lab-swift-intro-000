163815459cec05e7914c1354b37edbca2e5999ee
Deran Padayatchi <deran.padayatchi@fnb.co.za>
2018-03-22 14:28:36 +0200

    completed

--- LightBulb/ViewController.swift
+++ LightBulb/ViewController.swift
@@ -8,28 +8,43 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController
+{
     
     @IBOutlet weak var lightBulb: UIImageView!
+    @IBOutlet weak var segmentedControl: UISegmentedControl!
     
-    
-    override func viewDidLoad() {
-        
+    override func viewDidLoad()
+    {
         super.viewDidLoad()
         
-        lightBulb.backgroundColor = UIColor.blue
-        
+        self.changeLightBulbColor(color: "blue")
+        segmentedControl.selectedSegmentIndex = 2
     }
     
-    
-    @IBAction func colorSelected(_ sender: UISegmentedControl) {
-        
-        print(sender.selectedSegmentIndex)
-        
+    @IBAction func colorSelected(_ sender: UISegmentedControl)
+    {
+        self.changeLightBulbColor(color: sender.titleForSegment(at: sender.selectedSegmentIndex)!)
     }
     
-    
-
-   
-
+    func changeLightBulbColor(color: String)
+    {
+        var changeColor: UIColor = .white
+        
+        switch color.lowercased()
+        {
+        case "blue":
+         changeColor = .blue
+        case "red":
+            changeColor = .red
+        case "yellow":
+            changeColor = .yellow
+        case "green":
+            changeColor = .green
+        default:
+            changeColor = .white
+        }
+        
+        lightBulb.backgroundColor = changeColor
+    }
 }
